36ac0eda88815424321b6bd4ba709ef56091396f
CKronfeld <98053744+CKronfeld@users.noreply.github.com>
2022-02-10 12:33:50 -0600

    Delete H03 directory

--- H03/HO3-CH1.js
+++
@@ -1,9 +0,0 @@
- 
- 
- var floatMiles, floatGallons, floatMpg = parseFloat;
- 
-    floatMiles = parseFloat(prompt("Enter miles driven"));
-    
-    floatGallons = parseFloat(prompt("Enter gallons of gas used"));
-    floatMpg = floatMiles/floatGallons;
-    parseFloat(alert("Miles per gallon = " + floatMpg.toFixed(2)));
